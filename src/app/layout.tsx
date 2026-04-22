b74e1dce6a42169f92e8437e932d341c8aabc095
dam-eun <dameun0808@gmail.com>
2026-04-22 10:55:15 +0900

    [feat]구글 태그 매니저 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-KMQH37G5';
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID || 'G-77TJYZ919Q';
 
   return (
     <html lang="en">
@@ -58,6 +59,22 @@ export default function RootLayout({
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
               })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
             `,
           }}
         />
